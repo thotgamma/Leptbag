f89b22d20c6e175499f00f65166105f318c81f5f
thotgamma <gamma@e.thotgamma.com>
2017-10-09 03:04:08 +0900

    これでは落ちそうにない

--- friends/sharp20tester/source/main.d
+++ friends/sharp20tester/source/main.d
@@ -12,17 +12,42 @@ import dlib.math.quaternion;
 
 //ApplicationInterface----------------------
 
+elementNode block;
+int timer;
+
 extern (C) void init() {
 	rt_init();
 	try {
+		block = getCubeShape().generate(parameterPack(
+			param("position", Vector3f( 0, 10, 0)),
+			param("scale",    Vector3f(  1, 1, 1)),
+			param("rotation", Quaternionf(0, 0, 0, 1)),
+			param("mass", 1.0f)));
+
 
 	} catch (Exception ex){
 		writeln(ex.toString);
 	}
 }
 
+
 extern (C) void tick() {
 	try {
+
+		if (timer == 10) {
+			block.destroy();
+
+			block = getCubeShape().generate(parameterPack(
+				param("position", Vector3f( 0, 10, 0)),
+				param("scale",    Vector3f(  1, 1, 1)),
+				param("rotation", Quaternionf(0, 0, 0, 1)),
+				param("mass", 1.0f)));
+
+			timer = 0;
+		} else {
+			timer ++;
+		}
+
 
 	} catch (Exception ex){
 		writeln(ex.toString);
